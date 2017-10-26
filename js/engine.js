7c829a98d9f8c2f327a1e2a74f4d69da03639a35
Yuko Shiozaki <rio.wonderwoman@gmail.com>
2017-10-26 10:28:26 -0700

    Removing unnecessary comment

--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,6 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
-<<<<<<< HEAD
  * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
